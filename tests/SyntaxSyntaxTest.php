158fd7abf2dbf31d4b4ee54f8dbbb7587a12ec4a
Amine Ben hammou <webneat@gmail.com>
2016-07-19 00:31:55 +0200

    parse tests added

--- tests/SyntaxSyntaxTest.php
+++ tests/SyntaxSyntaxTest.php
@@ -10,104 +10,128 @@ use Tarsana\Syntax\Factory as S;
 
 class SyntaxSyntaxTest extends PHPUnit_Framework_TestCase {
 
-    public function test_parse() {
-        $ss = new SyntaxSyntax;
+    protected static $ss;
 
-        $s = $ss->parse('name');
-        $this->assertTrue($s instanceof StringSyntax);
-        $this->assertEquals('name', $s->description());
+    public static function setUpBeforeClass()
+    {
+        self::$ss = new SyntaxSyntax;
+    }
 
-        $s = $ss->parse('');
-        $this->assertTrue($s instanceof StringSyntax);
-        $this->assertEquals('', $s->description());
+    public static function tearDownAfterClass()
+    {
+        self::$ss = null;
+    }
 
-        $s = $ss->parse('[name]');
-        $this->assertTrue($s instanceof StringSyntax);
-        $this->assertEquals('name', $s->description());
-        $this->assertFalse($s->isRequired());
+    protected function checkParse($text, $class, $description, $isRequired) {
+        $s = self::$ss->parse($text);
+        $this->assertTrue($s instanceof $class);
+        $this->assertEquals($description, $s->description());
+        $this->assertEquals($isRequired, $s->isRequired());
+        return $s;
+    }
 
-        $s = $ss->parse('#number_of_numbers');
-        $this->assertTrue($s instanceof NumberSyntax);
-        $this->assertEquals('number_of_numbers', $s->description());
+    public function test_parse_string() {
+        $this->checkParse('name', StringSyntax::class, 'name', true);
+        $this->checkParse('', StringSyntax::class, '', true);
+        $this->checkParse('[name]', StringSyntax::class, 'name', false);
+    }
 
-        $s = $ss->parse('[#number_of_numbers]');
-        $this->assertTrue($s instanceof NumberSyntax);
-        $this->assertEquals('number_of_numbers', $s->description());
-        $this->assertFalse($s->isRequired());
+    public function test_parse_number() {
+        $this->checkParse('#my_number', NumberSyntax::class, 'my_number', true);
+        $this->checkParse('[#my_number]', NumberSyntax::class, 'my_number', false);
+    }
 
-        $s = $ss->parse('is-valid?');
-        $this->assertEquals('is-valid', $s->description());
-        $this->assertTrue($s instanceof BooleanSyntax);
+    public function test_parse_boolean() {
+        $this->checkParse('is-valid?', BooleanSyntax::class, 'is-valid', true);
+        $this->checkParse('[is-valid?]', BooleanSyntax::class, 'is-valid', false);
+    }
 
-        $s = $ss->parse('[is-valid?]');
-        $this->assertEquals('is-valid', $s->description());
-        $this->assertTrue($s instanceof BooleanSyntax);
-        $this->assertFalse($s->isRequired());
-
-        $s = $ss->parse('names[]');
-        $this->assertEquals('names', $s->description());
-        $this->assertTrue($s instanceof ArraySyntax);
+    public function test_parse_simple_array() {
+        // Array of strings with default separator
+        $s = $this->checkParse('names[]', ArraySyntax::class, 'names', true);
         $this->assertEquals(['foo', ' bar', 'baz'], $s->parse('foo, bar,baz'));
 
-        $s = $ss->parse('[names[]]');
-        $this->assertEquals('names', $s->description());
-        $this->assertTrue($s instanceof ArraySyntax);
-        $this->assertFalse($s->isRequired());
-        $this->assertEquals(['foo', ' bar', 'baz'], $s->parse('foo, bar,baz'));
-        $this->assertEquals('', $s->parse(''));
-
-        $s = $ss->parse('names[|]');
-        $this->assertEquals('names', $s->description());
-        $this->assertTrue($s instanceof ArraySyntax);
+        // Array of strings with custom separator
+        $s = $this->checkParse('names[|]', ArraySyntax::class, 'names', true);
         $this->assertEquals(['foo', ' bar', 'baz'], $s->parse('foo| bar|baz'));
 
-        $s = $ss->parse('#names[|]');
-        $this->assertEquals('names', $s->description());
-        $this->assertTrue($s instanceof ArraySyntax);
-        $this->assertEquals([1, 2, 3], $s->parse('1| 2|3'));
+        // Array of numbers with custom separator
+        $s = $this->checkParse('#numbers[|]', ArraySyntax::class, 'numbers', true);
+        $this->assertEquals([1, 3, 5], $s->parse('1|3|5'));
 
-        $s = $ss->parse('person{:,name,#age,friends[]}');
-        $this->assertEquals('person', $s->description());
-        $this->assertTrue($s instanceof ObjectSyntax);
+        // Array of booleans without description
+        $s = $this->checkParse('?[:]', ArraySyntax::class, '', true);
+        $this->assertEquals([true, false, true], $s->parse('true:no:yes'));
+
+        // Optional array of strings
+        $s = $this->checkParse('[names[]]', ArraySyntax::class, 'names', false);
+        $this->assertEquals(['foo', ' bar', 'baz'], $s->parse('foo, bar,baz'));
+        $this->assertEquals('', $s->parse(''));
+    }
+
+    public function test_parse_array_of_arrays() {
+        $s = $this->checkParse('#matrix[,][ ]', ArraySyntax::class, 'matrix', true);
+        $this->assertEquals([[1, 2], [3, 4]], $s->parse('1,2 3,4'));
+    }
+
+    public function test_parse_array_of_objects() {
+        $s = $this->checkParse('persons{name,#age}[]', ArraySyntax::class, 'persons', true);
+        $this->assertEquals([
+            (object) ['name' => 'Foo', 'age' => 26],
+            (object) ['name' => 'Bar', 'age' => 20]
+        ], $s->parse('Foo 26,Bar 20'));
+    }
+
+    public function test_parse_simple_object() {
+        // Simple Object
+        $s = $this->checkParse('person{:,name, #age, vip?,friends[]}', ObjectSyntax::class, 'person', true);
         $this->assertEquals(
-            (object) ['name' => 'Foo', 'age' => 12, 'friends' => ['Bar', 'Baz']],
-            $s->parse('Foo:12:Bar,Baz')
+            (object) ['name' => 'Foo', 'age' => 12, 'vip' => true, 'friends' => ['Bar', 'Baz']],
+            $s->parse('Foo:12:true:Bar,Baz')
         );
-
-        $s = $ss->parse('[person{:,name,#age,friends[]}]');
-        $this->assertEquals('person', $s->description());
-        $this->assertTrue($s instanceof ObjectSyntax);
-        $this->assertFalse($s->isRequired());
+        // Simple optional object
+        $s = $this->checkParse('[person{:,name,#age,friends[]}]', ObjectSyntax::class, 'person', false);
         $this->assertEquals(
             (object) ['name' => 'Foo', 'age' => 12, 'friends' => ['Bar', 'Baz']],
             $s->parse('Foo:12:Bar,Baz')
         );
         $this->assertEquals('', $s->parse(''));
+    }
 
-        $s = $ss->parse('{name}');
-        $this->assertEquals('', $s->description());
-        $this->assertTrue($s instanceof ObjectSyntax);
+    public function test_parse_object_with_one_field() {
+        // one required field
+        $s = $this->checkParse('{name}', ObjectSyntax::class, '', true);
         $this->assertEquals(
             (object) ['name' => 'me'],
             $s->parse('me')
         );
+    }
 
-        $s = $ss->parse('{[name]}');
-        $this->assertEquals('', $s->description());
-        $this->assertTrue($s instanceof ObjectSyntax);
+    public function test_parse_object_with_optional_fields() {
+        // one field and it's optional
+        // if all fields are optional, then the object is optional ?
+        $s = $this->checkParse('{[name]}', ObjectSyntax::class, '', true);
         $this->assertEquals(
             (object) ['name' => ''],
             $s->parse('')
         );
-
-        $s = $ss->parse('person{name,[#age],friends[]}');
-        $this->assertEquals('person', $s->description());
-        $this->assertTrue($s instanceof ObjectSyntax);
+        // one optional field among three
+        // using ' ' as default separator
+        $s = $this->checkParse('person{name,[#age],friends[]}', ObjectSyntax::class, 'person', true);
         $this->assertEquals(
             (object) ['name' => 'Foo', 'age' => '', 'friends' => ['Bar', 'Baz']],
             $s->parse('Foo Bar,Baz')
         );
+    }
 
+    public function test_parse_object_containing_objects() {
+        $s = $this->checkParse('user{name,accounts{:,site,login,[pass]}[]}', ObjectSyntax::class, 'user', true);
+        $this->assertEquals((object) ['name' => 'Foo', 'accounts' => [
+            (object) ['site' => 'fb', 'login' => 'foo', 'pass' => '***'],
+            (object) ['site' => 'gh', 'login' => 'mefoo', 'pass' => '']
+            ]],
+            $s->parse('Foo fb:foo:***,gh:mefoo')
+        );
     }
 
     public function test_dump() {
